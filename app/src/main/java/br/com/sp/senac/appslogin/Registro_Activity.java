b6cec45ba74ef916072eb092b9ff9476654658f1
laercio.nsilva <lndsilva@hotmail.com>
2020-03-03 10:57:11 -0300

    Initial commit

---
+++ app/src/main/java/br/com/sp/senac/appslogin/Registro_Activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.appslogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro_layout);
+    }
+}
